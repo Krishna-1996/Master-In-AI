9cc3f638841ae621d8aba548a9b40cfa7900772f
Krishna-1996 <1904krishna@gmail.com>
2024-11-30 21:33:11 +0000

    editing

--- Foundation of Artificial Intelligence/My Project Work/deletethis.py
+++ Foundation of Artificial Intelligence/My Project Work/deletethis.py
@@ -1,6 +1,6 @@
 # %%
 from pyamaze import maze
 m=maze(30,50)
-m.CreateMaze(loadMaze='D:/Masters Projects/Master-In-AI/Foundation of Artificial Intelligence/My Project Work/maze--2024-11-08--18-49-14.csv')
+m.CreateMaze(loopPercent=40, saveMaze=True)
 m.run()
 # %%
