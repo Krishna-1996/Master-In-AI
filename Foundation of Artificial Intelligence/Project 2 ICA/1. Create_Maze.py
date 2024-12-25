3a770252c00cf85d7bc32e78e90a074367f35039
Krishna-1996 <1904krishna@gmail.com>
2024-12-25 14:02:43 +0000

    editing

--- Foundation of Artificial Intelligence/Project 2 ICA/1. Create_Maze.py
+++ Foundation of Artificial Intelligence/Project 2 ICA/1. Create_Maze.py
@@ -1,8 +1,8 @@
 
 from pyamaze import maze
 
-my_Maze = maze(500, 500)
-my_Maze.CreateMaze(loopPercent= 10)
+my_Maze = maze(60, 150)
+my_Maze.CreateMaze(loopPercent=10, pattern= 'vertical')
 
 # Display the maze
 my_Maze.run()
