bb72a9837b526b5188abd4581b82a878d28e135f
Krishna-1996 <1904krishna@gmail.com>
2024-12-31 12:26:58 +0000

    Edit

--- Foundation of Artificial Intelligence/Project 2 ICA/2. Greedy_BFS/3.2 Greedy_BFS Weight_Change_Search_Length.py
+++ Foundation of Artificial Intelligence/Project 2 ICA/2. Greedy_BFS/3.2 Greedy_BFS Weight_Change_Search_Length.py
@@ -17,9 +17,9 @@ def chebyshev_heuristic(a, b):
 
 # Directional weights
 directional_weights = {
-    'N': -5,  # Moving north costs
-    'E': -5,  # Moving east costs
-    'S': 10,  # Moving south costs
+    'N': 10,  # Moving north costs
+    'E': -10,  # Moving east costs
+    'S': -10,  # Moving south costs
     'W': 10,  # Moving west costs
 }
 
