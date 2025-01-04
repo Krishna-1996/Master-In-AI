c5fa5c7755a4ddc9a6728188200bef94828c6f90
Krishna-1996 <1904krishna@gmail.com>
2025-01-04 08:36:12 +0000

    edit

---
+++ Foundation of Artificial Intelligence/Project 3 ICA/Advance Version of obstacle project/final_Project/test.py
@@ -0,0 +1,13 @@
+
+from pyamaze import maze
+
+# Create a maze object
+my_Maze = maze(50, 100)
+my_Maze.CreateMaze(loadMaze='D:/Masters Projects/Master-In-AI/Foundation of Artificial Intelligence/Project 3 ICA/Advance Version of obstacle project/final_Project/maze--2025-01-03--13-49-03.csv')
+
+my_Maze.run()
+# my_Maze.CreateMaze(loadMaze='D:/Masters Projects/Master-In-AI/Foundation of Artificial Intelligence/Project 3 ICA/normal Version/maze--2025-01-03--13-49-03.csv')
+
+# Display the maze
+my_Maze.run()
+# 'D:\Masters Projects\Master-In-AI\Foundation of Artificial Intelligence\Project 3 ICA\Advance Version of obstacle project\final_Project
