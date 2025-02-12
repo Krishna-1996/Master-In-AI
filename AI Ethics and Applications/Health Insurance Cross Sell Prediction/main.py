3a5d3f57563af905740b10691be5eb135186726d
Krishna-1996 <1904krishna@gmail.com>
2025-02-12 17:51:22 +0000

    editing

--- AI Ethics and Applications/Health Insurance Cross Sell Prediction/main.py
+++ AI Ethics and Applications/Health Insurance Cross Sell Prediction/main.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 
 # Load dataset (replace with your dataset)
-df = pd.read_csv('D:/Masters Projects/Master-In-AI/AI Ethics and Applications/Health Insurance Cross Sell Prediction.csv')
+df = pd.read_csv('D:/Masters Projects/Master-In-AI/AI Ethics and Applications/Health Insurance Main Dataset.csv')
 
 # Check for missing values
 df.isnull().sum()
